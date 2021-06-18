3c2a036807312bb394b950a479b06debe2e82791
KBENBENECK <kbenbeneck@gmail.com>
2021-06-18 15:03:33 -0400

    Wire up fetchPosts action creator. Call it on PostList mount.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
 class PostList extends React.Component {
+    componentDidMount() {
+        this.props.fetchPosts()
+    }
+    
     render() {
         return(
             <div>
@@ -9,4 +15,5 @@ class PostList extends React.Component {
         )
     }
 }
-export default PostList
+
+export default connect(null, {fetchPosts})(PostList)
