36ac2480b031f8f549bae10c0598d39d845d83cb
KBENBENECK <kbenbeneck@gmail.com>
2021-06-20 01:02:21 -0400

    Add postsReducer. Add mapState func. Render response data with style.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,14 +6,32 @@ class PostList extends React.Component {
     componentDidMount() {
         this.props.fetchPosts()
     }
-    
+    renderList() {
+        return this.props.posts.map(post => {
+            return (
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user" />
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+
+                    </div>
+                </div>
+            )
+        })
+    }
     render() {
+        
         return(
-            <div>
-                PostList
+            <div className="ui relaxed divided list">
+                {this.renderList()}
             </div>
         )
     }
 }
-
-export default connect(null, {fetchPosts})(PostList)
+const mapState = state => {
+    return { posts: state.posts}
+}
+export default connect(mapState, {fetchPosts})(PostList)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
 export default combineReducers({
-    placeholder: () => 'add some real reducers'
+    posts: postsReducer
 })
